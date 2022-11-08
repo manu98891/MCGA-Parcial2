1a01e3efc58418824261e936993fadc8a9230e68
Manuel Eugenio Losada Clua <ManuelEugenio.LosadaClua@endava.com>
2022-11-08 20:58:53 -0300

    Funciona por favor

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <h1>Hola mundo</h1>
-    <h1>Hola mundo</h1>
+    <h1>Hola  mundo</h1>
+    <h1>Hola  mundo</h1>
   </React.StrictMode>
 );
 
